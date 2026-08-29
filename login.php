8982636a3e341ba8c2ad7a1c531fa2730648f778
agrpkumara93-gif <agrpkumara93@gmail.com>
2026-08-29 12:05:41 +0530

    log file update

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login - Stationery Inventory</title>
+    <title>Login - Luma Bookshop & Communication</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
